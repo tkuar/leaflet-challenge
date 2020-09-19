033bce8404877d320ac1964bc7dbf4adcd097426
Tameka Kuar <tamekakuar17@gmail.com>
2020-09-19 19:43:58 -0400

    populate all layers on map

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,15 +1,33 @@
-  
-// Creating map object
-var myMap = L.map("map").setView([34, -96], 4.5);
+// Function to change circle marker color based on magnitude
+function chooseColor(mag) {
+    if (mag > 5) {
+        return "#780000";
+    }
+    else if (mag > 4) {
+        return "#dc0000";
+    }
+    else if (mag > 3) {
+        return "#fd8c00";
+    }
+    else if (mag > 2) {
+        return "#fdc500";
+    }
+    else if (mag > 1) {
+        return "#32CC32";
+    }
+    else {
+        return "#00ac46";
+    }
+}
 
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "light-v9",
-  accessToken: API_KEY
-}).addTo(myMap);
-
+// Create base layer
+// Grayscale Layer
+var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v9",
+    accessToken: API_KEY
+});
 // // Adding tile layer
 // L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -18,43 +36,52 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
 //   accessToken: API_KEY
 // }).addTo(myMap);
 
+// Create a layer group for earthquakes
+var earthquakes = L.layerGroup();
+// Create a baseMaps object
+var baseMaps = {
+    "Grayscale": graymap
+  };
+  
+  // Create an overlay object
+  var overlayMaps = {
+    "Earthquakes": earthquakes
+  };
+  // Creating map object
+var myMap = L.map("map",{
+    center: [34, -96],
+    zoom: 5,
+    layers: [graymap, earthquakes]
+});
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+  
+var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
+d3.json(geoData, function (data) {
 
-var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+    for (var i = 0; i < data.features.length; i++) {
+        // Set features' properties to a variable
+        var properties = data.features[i].properties;
+        // Set mag from properties to its own variable
+        var mag = properties.mag;
+        // Reformat the coordinates from features' geometry to [lat, lgn] and set to its own variable
+        var latlng = [data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]];
 
-function chooseColor(mag) {
-    if (mag > 5){
-        return "#ff3333";
+        // Create a circle markers for earthquakes
+        var markers = L.circle(latlng, {
+            color: "black",
+            fillColor: chooseColor(mag),
+            fillOpacity: 1,
+            weight: 0.5,
+            radius: mag * 30000
+        }).bindPopup(`<h4>Location: ${properties.place}<hr>Magnitude: ${mag}<br>When: ${Date(properties.time)}</h4>`).addTo(myMap);
+    
+        // Add marker to earthqakes overlayer
+        markers.addTo(earthquakes);
     }
-    else if (mag > 4){
-        return "#f5692e";
-    }
-    else if (mag > 3){
-        return "#ffa91f";
-    }
-    else if (mag > 2){
-        return "#fdac4e";
-    }
-    else if (mag > 1){
-        return "#b6f104";
-    }
-    else {
-        return "#1cd91c";
-    }
-}
-
-d3.json(geoData, function(data){
-    // Set properties from the data to a variable
-    var properties = data.features[1].properties;
-    // Set mag from properties to its own variable
-    var mag = properties.mag;
-    // Set only the lat and lgn from geometry to its own variable
-    var latlng = [data.features[1].geometry.coordinates[1],data.features[1].geometry.coordinates[0]];
-// Create a circle and pass in some initial options
-L.circle(latlng, {
-    color: "black",
-    fillColor: chooseColor(mag),
-    fillOpacity: 1,
-    radius: mag * 30000
-}).bindPopup("<h3>" + properties.place + "<br>Magnitude: " + mag).addTo(myMap);
+    
 });
