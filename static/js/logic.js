afac173565c70a160cb83d43abf0fec73f6742fd
Tameka Kuar <tamekakuar17@gmail.com>
2020-09-19 18:56:41 -0400

    update index.html and create working marker

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,4 @@
+  
 // Creating map object
 var myMap = L.map("map").setView([34, -96], 4.5);
 
@@ -17,11 +18,43 @@ L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?acce
 //   accessToken: API_KEY
 // }).addTo(myMap);
 
-// Create a circle and pass in some initial options
-L.circle([45.52, -122.69], {
-    color: "green",
-    fillColor: "green",
-    fillOpacity: 0.75,
-    radius: 500
-}).addTo(myMap);
+
+
 var geoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+
+function chooseColor(mag) {
+    if (mag > 5){
+        return "#ff3333";
+    }
+    else if (mag > 4){
+        return "#f5692e";
+    }
+    else if (mag > 3){
+        return "#ffa91f";
+    }
+    else if (mag > 2){
+        return "#fdac4e";
+    }
+    else if (mag > 1){
+        return "#b6f104";
+    }
+    else {
+        return "#1cd91c";
+    }
+}
+
+d3.json(geoData, function(data){
+    // Set properties from the data to a variable
+    var properties = data.features[1].properties;
+    // Set mag from properties to its own variable
+    var mag = properties.mag;
+    // Set only the lat and lgn from geometry to its own variable
+    var latlng = [data.features[1].geometry.coordinates[1],data.features[1].geometry.coordinates[0]];
+// Create a circle and pass in some initial options
+L.circle(latlng, {
+    color: "black",
+    fillColor: chooseColor(mag),
+    fillOpacity: 1,
+    radius: mag * 30000
+}).bindPopup("<h3>" + properties.place + "<br>Magnitude: " + mag).addTo(myMap);
+});
