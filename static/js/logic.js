4129f58a4158aded0b97e1e302844e9c44d24975
Tameka Kuar <tamekakuar17@gmail.com>
2020-09-19 20:50:52 -0400

    add legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -24,7 +24,9 @@ function chooseColor(mag) {
 // Grayscale Layer
 var graymap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    tileSize: 512,
     maxZoom: 18,
+    zoomOffset: -1,
     id: "light-v9",
     accessToken: API_KEY
 });
@@ -109,3 +111,22 @@ d3.json(url, function (data) {
         markers.addTo(earthquakes);
     }
 });
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function () {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 1,2,3,4,5];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + chooseColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
